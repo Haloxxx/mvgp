a488c02d9011d3affe028ae976b423cae36833e0
Wojciech Lange <wojtek.lange.gd@gmail.com>
2020-06-05 20:55:37 +0200

    commit

--- src/mvgp.jl
+++ src/mvgp.jl
@@ -1,5 +1,165 @@
 module mvgp
 
-greet() = print("Hello World!")
+"Finite central difference"
+fdcx(f, x, yc; h=1e-5) = (f(x+h/2, yc) - f(x-h/2, yc))/h
+
+fdcy(f, xc, y; h=1e-5) = (f(xc, y+h/2) - f(xc, y-h/2))/h
+
+fdcx3(f, x, yc, zc; h=1e-5) = (f(x+h/2, yc, zc) - f(x-h/2, yc, zc))/h
+
+fdcy3(f, xc, y, zc; h=1e-5) = (f(xc, y+h/2, zc) - f(xc, y-h/2, zc))/h
+
+fdcz3(f, xc, yc, z; h=1e-5) = (f(xc, yc, z+h/2) - f(xc, yc, z-h/2))/h
+
+function gradientDesc2(f, variables; e=1.0, β=0.01, ϵ=1e-5)
+    #println("y0: ", y0)
+    
+
+    x0 = variables[1]
+    y0 = variables[2]
+
+
+    F0 = f(x0, y0)
+    g0 = fdcx(f, x0, y0), fdcy(f, x0, y0)
+    dir = g0[1]*(-1), g0[2]*(-1)
+    xi = x0
+    yi = y0
+    #println("g0: ",g0)
+    var = 1
+    i = 1
+
+    while true
+
+        if(var == 1)
+            dir = g0[1]*(-1), g0[2]*(-1)
+        end
+        #println("dir2: ",dir[2])
+        xi_plus1 = xi + e*dir[1]
+        yi_plus1 = yi + e*dir[2]
+
+        F = f(xi_plus1, yi_plus1)
+        g = fdcx(f, xi_plus1, yi_plus1), fdcy(f, xi_plus1, yi_plus1)
+        #println("g: ", g)
+
+        if F < F0 && abs(F-F0)>1e-3
+            var = 1
+            g0 = g
+            #println("wariant1")
+            xi = xi_plus1
+            yi = yi_plus1
+        else
+            var = 2
+            trans = abs(g0[1]*g0[1]+g0[2]*g0[2])
+            if trans < ϵ
+                break
+                #println("koniec")
+            end
+
+            xi = xi_plus1 - e*dir[1]
+            yi = yi_plus1 - e*dir[2]
+
+            if(e - β > 0)
+                e -= β
+            end
+            #println("wariant2")
+        end
+
+        if i > 1000
+            break
+        end
+        i+=1
+    end
+    #println("xi: ",xi," yi: ",yi)
+    #println("")
+    return xi, yi
+    
+end
+
+function gradientDesc3(f, variables; e=1.0, β=0.01, ϵ=1e-5)
+    #println("y0: ", y0)
+    
+
+    x0 = variables[1]
+    y0 = variables[2]
+    z0 = variables[3]
+
+
+    F0 = f(x0, y0, z0)
+    g0 = fdcx3(f, x0, y0, z0), fdcy3(f, x0, y0, z0), fdcz3(f, x0, y0, z0)
+    dir = g0[1]*(-1), g0[2]*(-1), g0[3]*(-1)
+    xi = x0
+    yi = y0
+    zi = z0
+    #println("g0: ",g0)
+    var = 1
+    i = 1
+
+    while true
+
+        if(var == 1)
+            dir = g0[1]*(-1), g0[2]*(-1), g0[3]*(-1)
+        end
+        #println("dir2: ",dir[2])
+        xi_plus1 = xi + e*dir[1]
+        yi_plus1 = yi + e*dir[2]
+        zi_plus1 = zi + e*dir[3]
+
+        F = f(xi_plus1, yi_plus1, zi_plus1)
+        g = fdcx3(f, xi_plus1, yi_plus1, zi_plus1), fdcy3(f, xi_plus1, yi_plus1, zi_plus1), fdcz3(f, x0, y0, z0)
+        #println("g: ", g)
+
+        if F < F0 && abs(F-F0)>1e-3
+            var = 1
+            g0 = g
+            #println("wariant1")
+            xi = xi_plus1
+            yi = yi_plus1
+            zi = zi_plus1
+        else
+            var = 2
+            trans = abs(g0[1]*g0[1]+g0[2]*g0[2]+g0[3]+g0[3])
+            if trans < ϵ
+                break
+                #println("koniec")
+            end
+
+            xi = xi_plus1 - e*dir[1]
+            yi = yi_plus1 - e*dir[2]
+            zi = zi_plus1 - e*dir[3]
+
+            if(e - β > 0)
+                e -= β
+            end
+            #println("wariant2")
+        end
+
+        if i > 1000
+            break
+        end
+        i+=1
+    end
+    #println("xi: ",xi," yi: ",yi)
+    #println("")
+    return xi, yi, zi
+    
+end
+
+function gradientDesc(f, variables, n; e=1.0, β=0.01, ϵ=1e-5)
+    if β>=1 || β<=0
+        throw("0 < β < 1 has to be fulfilled")
+    end
+    result = 0
+    if n == 2
+        result = gradientDesc2(f, variables; e = e, β = β, ϵ = ϵ)
+    end
+
+    if n == 3
+        result = gradientDesc3(f, variables; e = e, β = β, ϵ = ϵ)
+    end
+
+    return result
+end
+
+export gradientDesc
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,70 @@
 using mvgp
 using Test
 
+Ackley(x, y) = -20*exp(-0.2*sqrt(0.5*(x^2+y^2)))-exp(0.5(cos(2*π*x)+cos(2*π*y)))+ℯ+20
+Beale(x,y) = (1.5 - x + x*y)^2 + (2.25 - x + x*y^2)^2 + (2.625 - x + x*y^3)^2
+Matyas(x,y) = 0.26(x^2 + y^2) - 0.48*x*y
+Booth(x,y) = (x+2*y-7)^2 + (2*x+y-5)^2
+Sphere2(x,y) = x^2+y^2
+Sphere3(x,y,z) = x^2+y^2+z^2
+Easom(x,y) = -cos(x)*cos(y)*exp(-((x-π)^2 + (y-π)^2))
+Levi(x,y) = sin(3*π*x)^2 + (x-1)^2*(1+sin(3*π*y)^2)+(y-1)^2*(1+sin(2*π*y)^2)
+McCormick(x,y) = sin(x+y)+(x-y)^2-1.5*x+2.5*y+1
+Booth_plus_z(x,y,z) = (x+2*y-7)^2 + (2*x+y-5)^2 + z^2
+
 @testset "mvgp.jl" begin
-    # Write your own tests here.
+
+    @testset "2 variables" begin
+
+        @testset "Sphere2" begin
+            for i in [1, 2, 3]
+                result = gradientDesc(Sphere2, (i, 2), 2)
+                @test isapprox(result[1], 0.0; atol=1e-6)
+                @test isapprox(result[2], 0.0; atol=1e-6)
+            end
+        end
+
+        @testset "Booth" begin
+            for i in [1, 2, 3]
+                #println("i: ",i)
+                result = gradientDesc(Booth, (i, 0.5), 2)
+                @test isapprox(result[1], 1.0; atol=1e-6)
+                @test isapprox(result[2], 3.0; atol=1e-6)
+            end
+        end
+
+        @testset "Matyas" begin
+            for i in [-0.55, -0.5, -0.45]
+                #println("i: ",i)
+                result = gradientDesc(Matyas, (i, -1.5), 2)
+                @test isapprox(result[1], 0; atol=1e-3)
+                @test isapprox(result[2], 0; atol=1e-3)
+            end
+        end
+
+    end
+    
+    @testset "3 variables" begin
+
+        @testset "Sphere3" begin
+            for i in [-0.55, -0.5, -0.45]
+                #println("i: ",i)
+                result = gradientDesc(Sphere3, (i, -1.5, 0), 3)
+                @test isapprox(result[1], 0; atol=1e-3)
+                @test isapprox(result[2], 0; atol=1e-3)
+                @test isapprox(result[3], 0; atol=1e-3)
+            end
+        end
+
+        @testset "Booth_plus_z" begin
+            for i in [-0.55, -0.5, -0.45]
+                #println("i: ",i)
+                result = gradientDesc(Booth_plus_z, (i, -1.5, 0), 3)
+                @test isapprox(result[1], 1; atol=1e-3)
+                @test isapprox(result[2], 3; atol=1e-3)
+                @test isapprox(result[3], 0; atol=1e-3)
+            end
+        end
+    end
+    
 end
